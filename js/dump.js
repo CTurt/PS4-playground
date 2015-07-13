a9183788527c93c1ead6cb34e31893ced8347ffe
CTurt <cturt@users.noreply.github.com>
2015-07-13 23:29:43 +0100

    Fix dumper

--- js/dump.js
+++ js/dump.js
@@ -1,44 +1,6 @@
-// This one usually stops dumping correct data and starts dumping just 00s halfway through
 function dump(base, chunkCount, chunkSize) {
-	var xx = base;
-	setBase(xx);
+	chunkSize /= 4;
 	
-	function du(bb) {
-		var dump_data = "";
-		
-		for(var aa = 0; aa < (chunkSize - 1); aa++) {
-			dump_data += u32[(bb * chunkSize) + aa].toString(16) +",";
-		}
-		
-		dump_data += u32[(chunkSize - 1)].toString(16);
-		
-		fname = "dump.php?name=dump-0x" + xx.toString(16) + "-" + bb;
-		$.ajax({
-			async : false,
-			url : fname,
-			type: "POST",
-			data : dump_data,
-			success: function(data, textStatus, jqXHR) {
-				logAdd("upload: success");
-				
-				if(bb < chunkCount) {
-					bb++;
-					du(bb);
-				}
-			},
-			error: function (jqXHR, textStatus, errorThrown) {
-				logAdd("upload fail: " + textStatus);
-			}
-		});
-	}
-	
-	du(0);
-}
-
-
-// This one usually crashes the browser halfway through
-/*
-function dump(base, chunkCount, chunkSize) {
 	var xx = base;
 	setBase(xx);
 	
@@ -66,4 +28,3 @@ function dump(base, chunkCount, chunkSize) {
 		});
 	}
 }
-*/
