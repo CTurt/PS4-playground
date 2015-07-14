792f2e17be10501b1fa7a6d6f4d76e5b80657368
CTurt <cturt@users.noreply.github.com>
2015-07-14 11:32:46 +0100

    Improve dumper reliability for large files

--- js/dump.js
+++ js/dump.js
@@ -1,18 +1,17 @@
 function dump(name, base, size) {
-	var chunkSize = 0x1000 / 4;
-	var chunkCount = size / chunkSize;
-	
-	var xx = base;
-	setBase(xx);
+	var chunkSize = 0x4000 / 4;
+	var chunkCount = (size / chunkSize) / 4;
 	
 	for(var bb = 0; bb < chunkCount; bb++) {
+		setBase(base + bb * chunkSize * 4);
+		
 		var dump_data = "";
 		
 		for(var aa = 0; aa < (chunkSize - 1); aa++) {
-			dump_data +=  "0x" + u32[(bb * chunkSize) + aa].toString(16) + ",";
+			dump_data +=  "0x" + u32[aa].toString(16) + ",";
 		}
 		
-		dump_data += u32[(chunkSize - 1)].toString(16);
+		dump_data += "0x" + u32[(chunkSize - 1)].toString(16);
 		
 		fname = "dump.php?name=" + name + "-" + bb;
 		$.ajax({
