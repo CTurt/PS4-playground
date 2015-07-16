ffbdd1c1bc245cf8e86aefa642e08c6aa2979d35
CTurt <ertl.chris@gmail.com>
2015-07-16 15:16:02 +0100

    Remove jquery

--- js/dump.js
+++ js/dump.js
@@ -1,5 +1,5 @@
 function dump(name, base, size) {
-	var chunkSize = 0x4000 / 4;
+	var chunkSize = 0x800 / 4;
 	var chunkCount = (size / chunkSize) / 4;
 	
 	for(var bb = 0; bb < chunkCount; bb++) {
@@ -7,24 +7,29 @@ function dump(name, base, size) {
 		
 		var dump_data = "";
 		
-		for(var aa = 0; aa < (chunkSize - 1); aa++) {
+		for(var aa = 0; aa < chunkSize - 1; aa++) {
 			dump_data +=  "0x" + u32[aa].toString(16) + ",";
 		}
 		
-		dump_data += "0x" + u32[(chunkSize - 1)].toString(16);
+		dump_data += "0x" + u32[chunkSize - 1].toString(16);
 		
 		fname = "dump.php?name=" + name + "-" + bb;
-		$.ajax({
-			async : false,
-			url : fname,
-			type: "POST",
-			data : dump_data,
-			success: function(data, textStatus, jqXHR) {
-				logAdd("upload: success");
-			},
-			error: function (jqXHR, textStatus, errorThrown) {
-				logAdd("upload fail: " + textStatus);
+		
+		var request = new XMLHttpRequest();
+		request.open("POST", fname, false);
+		request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		
+		request.onreadystatechange = function() {
+			if(request.readyState == 4) {
+				if(request.status == 200 || window.location.href.indexOf("http") == -1) {
+					logAdd("upload: success");
+				}
+				else {
+					logAdd("upload fail: " + textStatus);
+				}
 			}
-		});
+		}
+		
+		request.send(dump_data);
 	}
 }
